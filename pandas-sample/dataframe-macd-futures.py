5de18f56e444976fed48d3e1321fd5d08ca36806
John Sun <34933568+johnsun44@users.noreply.github.com>
2023-02-02 22:11:45 +0800

    change wording from "df" to "dataframe"

--- pandas-sample/dataframe-macd-futures.py
+++ pandas-sample/dataframe-macd-futures.py
@@ -5,7 +5,7 @@ class Strategy(StrategyBase):
         self.subscribed_books = {}
         self.options = {}
         self.data = {'open': [],'high': [],'low': [],'close': [],'std':[]}
-        self.df = pd.DataFrame(self.data)
+        self.dataframe = pd.DataFrame(self.data)
         # define your attributes here
         self.count=0
         self.last_type = 'none'
@@ -14,11 +14,11 @@ class Strategy(StrategyBase):
         exchange, pair, base, quote = CA.get_exchange_pair()
         if history_candles:     
             for candle in history_candles[exchange][pair]:
-                self.df=self.df.append({'open': self.count}, ignore_index=True)
-                self.df.open.loc[self.count] = candle['open']  
-                self.df.high.loc[self.count] = candle['high']
-                self.df.low.loc[self.count] = candle['low']
-                self.df.close.loc[self.count] = candle['close']
+                self.dataframe=self.dataframe.append({'open': self.count}, ignore_index=True)
+                self.dataframe.open.loc[self.count] = candle['open']  
+                self.dataframe.high.loc[self.count] = candle['high']
+                self.dataframe.low.loc[self.count] = candle['low']
+                self.dataframe.close.loc[self.count] = candle['close']
                 self.count += 1
     def on_order_state_change(self,  order):
         pass
@@ -28,11 +28,11 @@ class Strategy(StrategyBase):
         candle = candles[exchange][pair][0]
 
         #DIY pandas 
-        self.df=self.df.append({'open': self.count}, ignore_index=True)
-        self.df.open.loc[self.count]=candle['open']
-        self.df.high.loc[self.count]=candle['high']
-        self.df.low.loc[self.count]=candle['low']
-        self.df.close.loc[self.count]=candle['close']
+        self.dataframe=self.dataframe.append({'open': self.count}, ignore_index=True)
+        self.dataframe.open.loc[self.count]=candle['open']
+        self.dataframe.high.loc[self.count]=candle['high']
+        self.dataframe.low.loc[self.count]=candle['low']
+        self.dataframe.close.loc[self.count]=candle['close']
         self.count+=1        
 
         # get available balance
@@ -54,14 +54,14 @@ class Strategy(StrategyBase):
             available_short_position_size = short_position.available_size
             total_short_position_size = short_position.total_size
 
-        self.df['EMA_12'] = self.df.close.ewm(span=12).mean()
-        self.df['EMA_26'] = self.df.close.ewm(span=26).mean()
-        self.df['DIF'] =  self.df['EMA_12'] -  self.df['EMA_26']
-        self.df['DEM'] =  self.df['DIF'].ewm(span=9).mean()
-        self.df['OSC'] =  self.df['DIF'] -  self.df['DEM']
+        self.dataframe['EMA_12'] = self.dataframe.close.ewm(span=12).mean()
+        self.dataframe['EMA_26'] = self.dataframe.close.ewm(span=26).mean()
+        self.dataframe['DIF'] =  self.dataframe['EMA_12'] -  self.dataframe['EMA_26']
+        self.dataframe['DEM'] =  self.dataframe['DIF'].ewm(span=9).mean()
+        self.dataframe['OSC'] =  self.dataframe['DIF'] -  self.dataframe['DEM']
 
-        curr_macd =  self.df['OSC'].values[-1]
-        prev_macd = self.df['OSC'].values[-2]
+        curr_macd =  self.dataframe['OSC'].values[-1]
+        prev_macd = self.dataframe['OSC'].values[-2]
 
         signal = 0
         # MACD crosses the zero line from below - buy signal
